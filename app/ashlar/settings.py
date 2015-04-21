a8096770444e9eba94f4ee0e2c9ebed122d520e5
CloudNiner <andrew.fink.11@gmail.com>
2015-04-21 14:43:31 -0400

    Parametrize STATIC_ROOT django setting

--- app/ashlar/settings.py
+++ app/ashlar/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = secrets['ashlar_static_dir']
 
 # Django Rest Framework
 # http://www.django-rest-framework.org/
