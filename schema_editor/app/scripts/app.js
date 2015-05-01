39b594ff559010adc12a722d5056abb3935eb294
CloudNiner <andrew.fink.11@gmail.com>
2015-05-01 15:26:09 -0400

    Empty boilerplate grunt-angular app

---
+++ schema_editor/app/scripts/app.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    /**
+     * @ngdoc overview
+     * @name ase
+     * @description
+     * # ase: Ashlar Schema Editor
+     *
+     * Main module of the application.
+     */
+    angular.module('ase', []);
+
+})();
