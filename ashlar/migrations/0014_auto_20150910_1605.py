89c06d5996428a7b4fad7e1c0637e3b8961420a1
Kathryn Killebrew <kkillebrew@azavea.com>
2015-09-10 12:06:23 -0400

    Remove label and slug constant fields from records

---
+++ ashlar/migrations/0014_auto_20150910_1605.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ashlar', '0013_auto_20150514_1712'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='record',
+            options={'ordering': ('-created',)},
+        ),
+        migrations.RemoveField(
+            model_name='record',
+            name='label',
+        ),
+        migrations.RemoveField(
+            model_name='record',
+            name='slug',
+        ),
+    ]
