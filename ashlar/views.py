fe2accce616b51b4a59b425fc6d3d1a719665d15
Nathan Zimmerman <npzimmerman@gmail.com>
2015-10-12 14:06:02 -0400

    Add aggregation endpoint for recent counts

--- ashlar/views.py
+++ ashlar/views.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from datetime import datetime, timedelta
 
 from django.db import IntegrityError, connection
 
@@ -69,6 +70,36 @@ class RecordTypeViewSet(viewsets.ModelViewSet):
     queryset = RecordType.objects.all()
     serializer_class = RecordTypeSerializer
     filter_class = RecordTypeFilter
+
+    @detail_route(methods=['get'])
+    def recent_counts(self, request, pk=None):
+        """ Return the recent record counts for a given a record type
+        where recent = 30, 90, 365 days
+        """
+        record_type = RecordType.objects.get(pk=pk)
+        now = datetime.now()
+        durations = {
+            'month': 30,
+            'quarter': 90,
+            'year': 365
+        }
+
+        counts = {
+            'plural': record_type.plural_label,
+            'month': 0,
+            'quarter': 0,
+            'year': 0
+        }
+
+        for schema in record_type.schemas.all():
+            for label, days in durations.items():
+                earliest = now - timedelta(days=days)
+                count = (schema.record_set
+                         .filter(occurred_from__lte=now, occurred_from__gte=earliest).count())
+                counts[label] += count
+
+        # Construct JSON representation of `counts` for the response
+        return Response(counts)
 
 
 class SchemaViewSet(viewsets.GenericViewSet,
