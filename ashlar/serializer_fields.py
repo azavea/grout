7e635c9eee0361e3aa7b43086d92055fc2daab8d
Derek Dohler <ddohler@azavea.com>
2015-10-13 17:20:44 -0400

    Add no-geom view parameter and bounding box

--- ashlar/serializer_fields.py
+++ ashlar/serializer_fields.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.contrib.gis.geos import GEOSGeometry
 
 from rest_framework.fields import Field
 
@@ -30,3 +31,15 @@ class JsonSchemaField(JsonBField):
     """Json Field that also validates whether it is a JSON-Schema"""
     type_name = 'JsonSchemaField'
     validators = [validate_json_schema]
+
+
+class GeomBBoxField(Field):
+    """Serialize a geometry as a bounding box"""
+    read_only = True
+
+    def to_representation(self, value):
+        if not (issubclass(value.__class__, GEOSGeometry)):
+            msg = 'Can\'t apply GeomBBoxField to non-Geometry class {cls}'
+            raise ValidationError(msg.format(cls=value.__class__.__name__))
+        xmin, ymin, xmax, ymax = value.extent
+        return ({"lon": xmin, "lat": ymin}, {"lon": xmax, "lat": ymax})
---
+++ tests/test_serializer_fields_bbox.py
@@ -0,0 +1,21 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.contrib.gis.geos import Polygon, MultiPolygon
+
+from ashlar.serializer_fields import GeomBBoxField
+
+
+class GeomBBoxFieldTestCase(TestCase):
+    """ Test serializer field for JsonB """
+
+    def setUp(self):
+        self.bbox_field = GeomBBoxField()
+        self.poly = MultiPolygon(Polygon(((0, 0), (0, 1), (1, 1), (1, 0), (0, 0))))
+
+    def test_to_representation(self):
+        rep = self.bbox_field.to_representation(self.poly)
+        self.assertEqual(({"lat": 0.0, "lon": 0.0}, {"lat": 1.0, "lon": 1.0}), rep)
+
+    def test_validation(self):
+        with self.assertRaises(ValidationError):
+            self.bbox_field.to_representation("not a geometry")
