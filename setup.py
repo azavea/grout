cccca9c2feba5cbcb439f6d829e1e930819cb9c1
Kenny Shepard <kshepard@gmail.com>
2015-12-14 11:24:47 -0500

    Update djsonb, and add pytz

--- setup.py
+++ setup.py
@@ -13,19 +13,20 @@ setup(
     keywords='gis jsonschema',
     packages=find_packages(exclude=['tests']),
     dependency_links=[
-        'https://github.com/azavea/djsonb/tarball/develop#egg=djsonb-0.1.4'
+        'https://github.com/azavea/djsonb/tarball/develop#egg=djsonb-0.1.5'
     ],
     install_requires=[
         'Django ==1.8.6',
         'djangorestframework >=3.1.1',
         'djangorestframework-gis >=0.8.1',
         'django-filter >=0.9.2',
-        'djsonb >=0.1.4',
+        'djsonb >=0.1.5',
         'jsonschema >=2.4.0',
         'psycopg2 >=2.6',
         'django-extensions >=1.5.2',
         'python-dateutil >=2.4.2',
-        'PyYAML >=3.11'
+        'PyYAML >=3.11',
+        'pytz >= 2015.7'
     ],
     extras_require={
         'dev': [],
